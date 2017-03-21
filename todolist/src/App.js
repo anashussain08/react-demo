421046af94870d80fc8f5653b2b339cb8623f97e
anashussain08 <anas.hussain08@gmail.com>
2017-03-21 10:32:46 +0500

    changes

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import InputComponent from './components/input/input'
 import ViewComponent from './components/view/view'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      newTodo : '',
+      allTodos:[]
+    }
+  }
+   addTodo(){
+        console.log(this.state.newTodo);
+        const todolist = this.state.allTodos;
+        todolist.push(
+          {
+            task:this.state.newTodo,
+            completed:false,
+            createdAt:new Date().getTime()
+          }
+        );
+        console.log(this.state.allTodos);
+    }
+    ChangeVal(ev){
+        this.setState({newTodo:ev.target.value});
+    }
+    getTodos(){
+      return this.state.allTodos;
+    }
   render() {
     return (
       <div className="App">
@@ -13,9 +37,15 @@ class App extends Component {
           <h2>Todo List</h2>
         </div>
         
-        <InputComponent />
+         <div>
+            <h2>Enter your Todo's here</h2>
+            <div className="input-group col-md-3 col-xs-6 mar">
+                <input type="text" value={this.newTodo} onChange={this.ChangeVal.bind(this)} className="form-control" placeholder="todos.." />
+                <button onClick={this.addTodo.bind(this)} className="btn btn-danger" type="button">Add</button>
+            </div>
+        </div>
         <hr />
-        <ViewComponent />
+        <ViewComponent todolist={this.getTodos()}/>
       </div>
     );
   }
--- todolist/src/components/view/view.js
+++ todolist/src/components/view/view.js
@@ -3,12 +3,37 @@ import './view.css';
 
 
 class ViewComponent extends Component{
+   showList(){
+       return (
+            this.props.todolist.map((item,ind)=>{
+                return (
+                        <li className="ui-state-default">
+                                <div className="checkbox">
+                                    <label>
+                                        <input type="checkbox" value="" />item.task</label>
+                                        <a href='#' className='close' aria-hidden='true'>&times;</a>
+                                </div>
+                                <hr />
+                        </li>
+                )
+            })
+       )
+   }
 render(){
     return (
     <div>
-        <h1>ViewComponent</h1>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-3'>
+                      <ul id="sortable" className="list-unstyled">
+                        {this.showList()}
+                    </ul>
+                </div>
+                
+            </div>
+        </div>
     </div>
-    )
+    )   
 }
 }
 
